05167521ebf459840be8c60fe54414c902442454
NotAditya69 <90441096+NotAditya69@users.noreply.github.com>
2021-10-25 09:24:42 +0530

    Update afk.js

--- commands/info/afk.js
+++ commands/info/afk.js
@@ -11,18 +11,18 @@ module.exports = {
      * @param {String[]} args 
      */
     run: async (client, message, args) => {
-    const afkreason = args.join(" ") || 'No reason!';
-    db.findOne({ Guild: message.guildId, Member: message.author.id }, async(err, data) => {
-      if(data) {
-        return;
-      } else {
-        Data = new db({
+    const afkreason = args.join(" ") || 'No reason!';// abhi slash ka alag hai ye iska mtlb hai reason dhund na always remember if kuch content import karoge to use this
+    db.findOne({ Guild: message.guildId, Member: message.author.id }, async(err, data) => { //ye guild id and author id dhundega so data store kare 
+      if(data) { //and once data is stored
+        return;// wapas ayega
+      } else {// and then
+        Data = new db({//new data banega and uspe iska server id and user id store hoga also afk reason jo hamne var banaya tha udhar se reason leke store karega so banda wapas ana pa afk hata de
           Guild: message.guildId,
           Member: message.author.id,
           Content: afkreason,
-          TimeAgo: Date.now()
+          TimeAgo: Date.now()// date/time yaad rakhega
         })
-        Data.save()
+        Data.save()// and then data save kardega and embed send kardega
         const afksave = new MessageEmbed()
         .setColor('RANDOM')
         .setDescription(`You have been set to AFK\n**Reason:** ${afkreason}`)
