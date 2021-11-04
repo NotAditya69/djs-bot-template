6126fa86688f468074e060424c42e9c34eb95738
NotAditya69 <90441096+NotAditya69@users.noreply.github.com>
2021-11-04 23:27:56 +0530

    Update set-welcome.js

--- commands/info/set-welcome.js
+++ commands/info/set-welcome.js
@@ -11,22 +11,22 @@ module.exports = {
      * @param {String[]} args 
      */
      run: async (client, message, args) => {
-         if (!message.member.permissions.has("ADMINISTRATOR")) return; // this means if the user don't have perms then he can't do hoti welcome
+         if (!message.member.permissions.has("ADMINISTRATOR")) return; 
 
-         const channel = message.mentions.channels.first(); //this means jo bhi banda channel name dalega for setup wo ye dhund lega
-         if (!channel) return message.reply("Please mention a channel!"); // And agar channel name nahi dalta then ye bolega chutiye channel name dalna
+         const channel = message.mentions.channels.first(); 
+         if (!channel) return message.reply("Please mention a channel!"); 
 
-         Schema.findOne({ Guild: message.guild.id }, async (err, data) => { // this is mongoose data store ninja technique
-             if (data) { // this means ye saab set channel wala kam jo hamne kiya wo store karega and isse work karne ke liye hame ek schema chahiye aka store kaha karna hai and wo models ke help se karenga
-                 data.channel = channel.id //data.channel mongoose ke language mai bolu to mongoose yaad rakhega konse channel pa welcome bhejna hai and usse hamne bataya wo chiz by using channel.id and models pa define kiya
-                 data.save();// this means ye channel setup karne ke baad ye data store hoga and save hoga so mongoose bhul na jaye kaha bhejna hai welcome
+         Schema.findOne({ Guild: message.guild.id }, async (err, data) => { 
+             if (data) { 
+                 data.channel = channel.id
+                 data.save();
              } else {
-                 new Schema({ // ye naya schema banega for events to work samjhe
+                 new Schema({ 
                      Guild: message.guild.id,
                      Channel: channel.id,
                  }).save();
              }
-             message.reply(`${channel} has been set as the welcome channel`)// then after this ye msg milega ki ho gaya chacha
+             message.reply(`${channel} has been set as the welcome channel`)
          });
 
      },
