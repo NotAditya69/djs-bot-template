c133478b8b670a4255fd6ccab8f56aee9c37817c
NotAditya <90441096+NotAditya69@users.noreply.github.com>
2021-12-20 20:45:09 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,34 +1,25 @@
-const Discord = require(`discord.js`);
-const { MessageEmbed } = require('discord.js');
+const Discord = require("discord.js") // discord imported ez 
+const client = require("../index") // client ko index so import karliya cuz client is defined as client constructor
 
-/**
- * @param {Discord.Client} client
- */
-module.exports = (client) => {
-    client.on(`ready`, () => {
-        const embed = new MessageEmbed()
-        .setTitle('BOT STATUS')
-        .setDescription(`**Status**: ${client.user.tag} is now online!\n**Ping**: ${client.ws.ping}`)
-        .setFooter('Owner: Aditya')
-        .setColor('RANDOM')
-        client.channels.cache.get(`900664688768057385`).send({embeds: [embed]});
-        console.log(`${client.user.tag} is now online!✅`)
+client.on("ready", () => {
+    console.log(`${client.user.username} Is Now Online MFS!`)
+const activity = [
+    "Your mom",
+    "Your dad",
+    "Anurag's PP",
+]
+const type = [
+    "WATCHING",
+    "LISTENING",
+    "COMPETING"
+]
+setInterval(() => client.user.setPresence({
+    activities: [{
+        name: activity[Math.floor(Math.random * activity.length)]
+        type: type[Math.floor(Math.random * type.length)]
+}],
+                                          status: 'dnd'
+}), 5000)
+})
 
-        setInterval(() => {
-            client.user.setPresence({ activities: [{ name: `in ${client.guilds.cache.size} servers! | ${client.prefix}help` }] });
-        }, 10000)
-
-        const { inspect } = require("util")
-        process.on(`unhandledRejection`, (reason, promise) => {
-            client.channels.cache.get(`899160781147496499`).send(`**UnhandledRejection**\n**Reason:**\n\`\`\`\n${inspect(reason, { depth: 0 })}\n\`\`\` Promise:\n\`\`\`\n${inspect(promise, { depth: 0 })}\n\`\`\``)
-        })
-        process.on(`uncaughtException`, (err, origin) => {
-            client.channels.cache.get(`899160781147496499`).send(`**UncaughtException**\n**Error:**\n\`\`\`\n${inspect(err, { depth: 0 })}\n\`\`\`\nType: ${inspect(origin, { depth: 0 })}`)
-        })
-        process.on(`warning`, (warn) => {
-            client.channels.cache.get(`899160781147496499`).send(`**Warning\n**Warn:**\n\`\`\`\n${warn.name}\n${warn.message}\n\n${warn.stack}\n\`\`\``)
-        })
-
-    })
-}
-
+/* Your Mom */
